e29dc6ee456e471b2c06ec455a0c040fcb413f88
João Vieira <joaolcv1@gmail.com>
2020-04-07 11:41:44 -0300

    Desafio1

---
+++ index.js
@@ -0,0 +1,15 @@
+const nome = 'Joao'
+const peso = 74
+const altura = 1.80
+const imc = peso/ (altura*altura)
+const n = 2*2
+if(imc >= 30) {
+   console.log(`${nome} você está acima do peso`)
+} else {
+   console.log(`${nome} você não está acima do peso`)
+
+}
+console.log(n)
+
+
+
